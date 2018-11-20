6d4c87915f82a05db89fc9241aaa44e3136dcc43
gm2552 <gm2552@localhost>
2018-11-20 08:38:50 -0600

    Removing old web.xml and spring xml files.
    Adding default config service url and security credentials.
    Updating to Greenwich.M2

--- src/main/java/org/nhind/xdm/springconfig/CXFWSServiceConfig.java
+++ src/main/java/org/nhind/xdm/springconfig/CXFWSServiceConfig.java
@@ -9,7 +9,9 @@ import org.nhind.xdr.XDR;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class CXFWSServiceConfig
 {
 	@Autowired
--- src/main/java/org/nhind/xdm/springconfig/WebSecurityConfiguration.java
+++ src/main/java/org/nhind/xdm/springconfig/WebSecurityConfiguration.java
@@ -20,6 +20,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception
     {
         http.authorizeRequests().antMatchers("/").permitAll();
+        
+        http.csrf().disable();
     }
 }
 
